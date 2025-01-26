c848ef497ee17eb56f7c2d4d6cedaa1f3d9755bd
weck25 <weck2505@gmail.com>
2025-01-26 08:29:45 +0700

    fix image

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    domains: ['gateway.pinata.cloud'],
     remotePatterns: [
       {
         protocol: 'https',
